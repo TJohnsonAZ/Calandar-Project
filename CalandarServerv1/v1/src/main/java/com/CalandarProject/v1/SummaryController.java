c940636fcdcd58efe3555580668d70169f36ec52
Trevor Johnson <tkj44@nau.edu>
2021-04-10 22:35:35 -0700

    commented summary classes and fixed streak error

--- CalandarServerv1/v1/src/main/java/com/CalandarProject/v1/SummaryController.java
+++ CalandarServerv1/v1/src/main/java/com/CalandarProject/v1/SummaryController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 public class SummaryController {
 
+    // get request handler that creates and returns a summary object using given data
     @GetMapping("/summary")
     public Summary summary(@RequestParam(value = "startDate") int start,
                            @RequestParam(value = "endDate") int end,
