93378fd6a35d88e2f25a32156f7ec9479719d9df
Trevor Johnson <tkj44@nau.edu>
2021-04-17 16:09:25 -0700

    Summary tests and bug fix

---
+++ CalandarServerv1/v1/src/main/java/com/CalandarProject/v1/Summary/SummaryTest.java
@@ -0,0 +1,85 @@
+package com.CalandarProject.v1.Summary;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import com.CalandarProject.v1.DayData.DayData;
+import com.CalandarProject.v1.DayData.DayDatabase;
+
+public class SummaryTest {
+
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+        System.out.println("Begin Summary Tests\n");
+    }
+
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+        System.out.println("\nEnd Summary Tests");
+    }
+
+    @Test
+    public void summaryGetCompleteCounter() {
+        DayData day = new DayData("6527", "7", "3", "1", "2", "3");
+        DayDatabase database = new DayDatabase();
+        database.addDayData(day);
+        Summary summary = new Summary(7, 7, "6527");
+        int[] output = summary.getCompleteCounter();
+        assertEquals(1, output[0]);
+    }
+
+    @Test
+    public void summaryGetIncompleteCounter() {
+        DayData day = new DayData("6527", "7", "3", "1", "2", "3");
+        DayDatabase database = new DayDatabase();
+        database.addDayData(day);
+        Summary summary = new Summary(7, 7, "6527");
+        int[] output = summary.getIncompleteCounter();
+        assertEquals(1, output[2]);
+    }
+
+    @Test
+    public void summaryGetClearCounter() {
+        DayData day = new DayData("6527", "7", "3", "1", "2", "3");
+        DayDatabase database = new DayDatabase();
+        database.addDayData(day);
+        Summary summary = new Summary(7, 7, "6527");
+        int[] output = summary.getClearCounter();
+        assertEquals(1, output[1]);
+    }
+
+    @Test
+    public void summaryGetHighestStreak() {
+        DayData day = new DayData("6527", "8", "3", "1", "1", "0");
+        DayDatabase database = new DayDatabase();
+        database.addDayData(day);
+        Summary summary = new Summary(8, 8, "6527");
+        int[] output = new int[4];
+        output = summary.getHighestStreak();
+        assertEquals(0, output[2]);
+    }
+
+    @Test
+    public void summaryGetNumActivities() {
+        DayData day = new DayData("6527", "8", "3", "1", "1", "0");
+        DayDatabase database = new DayDatabase();
+        database.addDayData(day);
+        Summary summary = new Summary(8, 8, "6527");
+        int output = summary.getNumActivities();
+        assertEquals(3, output);
+    }
+
+    @Test
+    public void summaryGetHighestOverallStreak() {
+        DayData day = new DayData("6527", "7", "3", "1", "2", "3");
+        DayDatabase database = new DayDatabase();
+        database.addDayData(day);
+        day = new DayData("6527", "8", "3", "1", "1", "0");
+        database.addDayData(day);
+        Summary summary = new Summary(7, 8, "6527");
+        int output = summary.getHighestOverallStreak();
+        assertEquals(2, output);
+    }
+}
